2af82e85f8f12c5d04e29152cf6482753a660fb3
salif <tanksalif@gmail.com>
2025-06-15 12:53:06 -0400

    add gb experience exporter

---
+++ src/endpoints/seed/gb-experience.ts
@@ -0,0 +1,13 @@
+import type { Media } from '@/payload-types'
+
+export const gbExperience1: Omit<Media, 'createdAt' | 'id' | 'updatedAt'> = {
+  alt: 'Gems & Glamour: A Jewelry Showcase',
+}
+
+export const gbExperience2: Omit<Media, 'createdAt' | 'id' | 'updatedAt'> = {
+  alt: 'Sparkle & Shine Jewelry Showcase',
+}
+
+export const gbExperience3: Omit<Media, 'createdAt' | 'id' | 'updatedAt'> = {
+  alt: 'Magic & Mystery: A Jewelry Showcase',
+}
