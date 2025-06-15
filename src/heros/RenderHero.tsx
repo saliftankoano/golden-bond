a31b7f98da7a9d8eba6655d4ff9e2ad55e192e7a
salif <tanksalif@gmail.com>
2025-06-15 11:46:19 -0400

    remove unsed renderhero highimpact types

--- src/heros/RenderHero.tsx
+++ src/heros/RenderHero.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 
 import type { Page } from '@/payload-types'
 
-import { HighImpactHero } from '@/heros/HighImpact'
-import { LowImpactHero } from '@/heros/LowImpact'
-import { MediumImpactHero } from '@/heros/MediumImpact'
+import { LandingHero } from '@/heros/LandingHero'
 
 const heroes = {
-  highImpact: HighImpactHero,
-  lowImpact: LowImpactHero,
-  mediumImpact: MediumImpactHero,
+  landingHero: LandingHero,
 }
 
 export const RenderHero: React.FC<Page['hero']> = (props) => {
