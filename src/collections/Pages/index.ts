b9219656801bf611fa5c3be68652f3384cd7134c
salif <tanksalif@gmail.com>
2025-06-15 11:45:30 -0400

    add pages index

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -7,6 +7,8 @@ import { CallToAction } from '../../blocks/CallToAction/config'
 import { Content } from '../../blocks/Content/config'
 import { FormBlock } from '../../blocks/Form/config'
 import { MediaBlock } from '../../blocks/MediaBlock/config'
+import { Services } from '../../blocks/Services/config'
+import { Experience } from '../../blocks/Experience/config'
 import { hero } from '@/heros/config'
 import { slugField } from '@/fields/slug'
 import { populatePublishedAt } from '../../hooks/populatePublishedAt'
@@ -75,7 +77,7 @@ export const Pages: CollectionConfig<'pages'> = {
             {
               name: 'layout',
               type: 'blocks',
-              blocks: [CallToAction, Content, MediaBlock, Archive, FormBlock],
+              blocks: [CallToAction, Content, MediaBlock, Archive, FormBlock, Services, Experience],
               required: true,
               admin: {
                 initCollapsed: true,
